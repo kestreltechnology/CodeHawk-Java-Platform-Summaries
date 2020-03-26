00003bf7472d5bafbc2b748a10eaf23da7fa15db
Andrew McGraw <mcgraw4@illinois.edu>
2020-03-25 18:05:49 -0700

    rename scripts

--- profiling/ByteCode_Timing/profile_bytecode.py
+++ profiling/ByteCode_Timing/profile_bytecode.py
@@ -13,10 +13,6 @@ def parse():
     parser.add_argument('bytecode',help='Fully qualified classname of interest')
     #parser.add_argument('method',help='Method of interest')
     parser.add_argument('--values',nargs='*',default=[],help='Input arguments to pass to the method')
-    #parser.add_argument('@@str_values',nargs='*',default=[],help='Input argument to pass to the method')
-    #parser.add_argument('@@input_file',default=None,help='Input file to parse')
-    #parser.add_argument('@@output_file',default=None,help='Output file to write results')
-    #parser.add_argument('@@outer_loops',default=None,help='Outer loops to use')
     args = parser.parse_args()
     return args
 
@@ -27,13 +23,8 @@ if __name__ == '__main__':
     java_cmd = [ 'java', '-Xmx128m', '-Xint', '-classpath', '../lib/commons-cli-1.3.jar:bin/', 'ByteCode_Timing/ByteCode_Timing' ]
     java_cmd.extend(['-r', args.repeats])
     java_cmd.extend(['-b', args.bytecode])
-    #for value in args.values:
-    #    java_cmd.extend(['-v', value])
     for value in args.values:
         java_cmd.extend(['-v', value])
-    #for value in args.str_values:
-    #    java_cmd.extend(['-s', value])
-    #java_cmd.extend(['-v'])
 
     print java_cmd
     subprocess.call(java_cmd)
